584c944754600f9ca95722b1b168fecf48acfdb0
Beranger Scherpereel <beranger.scherpereel@gmail.com>
2022-11-25 16:05:20 +0100

    edit list Pokemon

--- pokemon/resources/views/listePokemons.blade.php
+++ pokemon/resources/views/listePokemons.blade.php
@@ -3,33 +3,34 @@
 @section('style')
 
     <link rel="stylesheet" href="//cdn.datatables.net/1.13.1/css/jquery.dataTables.min.css" >
+    <link href="css/content.css" rel="stylesheet" />
 @endsection
 
 @section('content')
-    <div><p>Liste Pokemon </p></div>
+    <div class="content">
+        <table id="myTable"  style="width:100%">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Name</th>
+                    <th >Image</th>
+                    <th >Energy</th>
+                
+                </tr>
+            </thead>
 
-    <table id="myTable"  style="width:100%">
-        <thead>
-            <tr>
-                <th>Id</th>
-                <th>Name</th>
-                <th >Image</th>
-                <th >Energy</th>
             
-            </tr>
-        </thead>
-
-        
-        <tbody>
-            <tr>
-                @foreach ($Pokemons as $pokemon)
-                    <td> {{$pokemon->id}}</td>
-                    <td> {{$pokemon->name}}</td>
-                    <td><img src= "{{$pokemon->path}}"></td>
-                    <td> {{$pokemon->energy->name}}</td></tr>
-                @endforeach 
-        </tbody>
-    </table>
+            <tbody>
+                <tr>
+                    @foreach ($Pokemons as $pokemon)
+                        <td> {{$pokemon->id}}</td>
+                        <td> {{$pokemon->name}}</td>
+                        <td><img src= "{{$pokemon->path}}"></td>
+                        <td> {{$pokemon->energy->name}}</td></tr>
+                    @endforeach 
+            </tbody>
+        </table>
+    </div>
 @endsection
 
 @section('script')

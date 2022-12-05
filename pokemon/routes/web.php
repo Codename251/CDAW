dfa1ec01267a2acf7340cc068b18ffdd462deb31
Beranger Scherpereel <beranger.scherpereel@gmail.com>
2022-12-05 10:22:35 +0100

    test auth

--- pokemon/routes/web.php
+++ pokemon/routes/web.php
@@ -54,11 +54,8 @@ Route::get('/htmlTest', function () {
 // });
 
 Route::get('/listePokemons', 'App\Http\Controllers\listePokemonsController@getListePokemons');
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
+
+Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');

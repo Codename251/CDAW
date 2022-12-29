c1a62cbf0d9f356c0b3e1f781a5ac469249fc24a
Beranger Scherpereel <beranger.scherpereel@gmail.com>
2022-12-29 18:47:26 +0100

    Ajout MCD MLD version finale

--- pokemon2/resources/views/auth/register.blade.php
+++ pokemon2/resources/views/auth/register.blade.php
@@ -13,7 +13,7 @@
             @csrf
 
             <div>
-                <x-jet-label for="name" value="{{ __('Name') }}" />
+                <x-jet-label for="name" value="{{ __('Pseudo') }}" />
                 <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             </div>
 
